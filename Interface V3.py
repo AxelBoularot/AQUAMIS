2dd3e4925cf83501d22be0e6458d00bc68438064
pilangele90-web <pilangele90@gmail.com>
2025-11-07 18:30:39 +0100

    Test Commit

--- Interface V3.py
+++ Interface V3.py
@@ -5,7 +5,6 @@ from progress_bar import ProgressBar
 from Graphs_Data import GraphManager
 from threading import Thread
 from Graphs_Main import Graphs_Main
-from tkinter import messagebox
 
 # Pygame's initialization - AMIS' LOGO - Interface's name
 
